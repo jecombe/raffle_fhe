5a36b7efd712742dc4cce37bd0f3ba9427444bf7
jecombe <jeremcombe@hotmail.fr>
2024-10-05 01:56:39 +0200

    add(tests): adding test to pick a winner

--- contracts/test/raffle/TicketFactory.ts
+++ contracts/test/raffle/TicketFactory.ts
@@ -7,12 +7,20 @@ import { getSigners, initSigners } from "../signers";
 import { deployFactoryFixture } from "./TicketFactory.fixture";
 
 describe("TicketFactory", function () {
+  let owner;
+  let participant1, participant2;
+
   before(async function () {
     await initSigners();
     this.signers = await getSigners();
   });
 
   beforeEach(async function () {
+    [owner, participant1, participant2] = await ethers.getSigners();
+    this.owner = owner;
+    this.participant1 = participant1;
+    this.participant2 = participant2;
+
     const contract = await deployFactoryFixture();
     this.contractAddress = await contract.getAddress();
     this.factory = contract;
@@ -129,5 +137,43 @@ describe("TicketFactory", function () {
         encryptedTransferAmount.inputProof,
       ),
     ).to.be.revertedWith("Raffle has ended");
+  });
+
+  describe("pickWinner", function () {
+    it("should fail if the tombola is already finished", async function () {
+      // Start the tombola
+      await this.ticketContract.start(1, 20, 2); // Set a ticket price, duration, and limited tickets
+      const transaction = await this.erc20.mint(10000);
+      await transaction.wait();
+
+      const input = this.instances.alice.createEncryptedInput(this.ticketAddress, this.signers.alice.address);
+      input.addAddress(this.signers.alice.address).add64(1);
+      const encryptedTransferAmount = input.encrypt();
+
+      const txToken = await this.erc20["approve(address,bytes32,bytes)"](
+        this.ticketAddress,
+        encryptedTransferAmount.handles[1],
+        encryptedTransferAmount.inputProof,
+      );
+      await txToken.wait();
+
+      const txTicket = await this.ticketContract["buyTicket(bytes32,bytes32,bytes)"](
+        encryptedTransferAmount.handles[0],
+        encryptedTransferAmount.handles[1],
+        encryptedTransferAmount.inputProof,
+      );
+      await txTicket.wait();
+
+      await new Promise((resolve) => setTimeout(resolve, 20 * 1000)); // wait 20 secondes
+
+      await this.ticketContract.pickWinner();
+
+      const winnerAddress = await this.ticketContract.winnerDecrypt();
+      console.log(winnerAddress);
+      expect(winnerAddress).to.satisfy((addr: any) => {
+        return ethers.isAddress(addr);
+    }, "winnerDecrypt is not a valid address");
+
+    });
   });
 });
