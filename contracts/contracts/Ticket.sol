fda6caa1d25cb4ce8bcebf219a626909aae61655
jecombe <jeremcombe@hotmail.fr>
2024-10-04 16:59:35 +0200

    feat:  add Ticket contract with encrypted ERC20

--- contracts/contracts/Ticket.sol
+++ contracts/contracts/Ticket.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.8.24;
 
 import "fhevm/lib/TFHE.sol";
 import "fhevm/gateway/GatewayCaller.sol";
+import "./interfaces/IEncryptedERC20.sol";
 
-contract Ticket is GatewayCaller {
+import "./encryptedErc20/EncryptedERC20.sol";
 
-    constructor() {
+contract Ticket is GatewayCaller, EncryptedERC20 {
+    IEncryptedERC20 public token;
+
+    constructor(
+        address _owner,
+        uint amount,
+        string memory _name,
+        string memory _symbol,
+        address _tokenAddress
+    ) EncryptedERC20(_name, _symbol) {
+        token = IEncryptedERC20(_tokenAddress);
     }
 }
