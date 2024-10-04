111e7d380ed44d02fbb1428e6bc52da2bf074271
jecombe <jeremcombe@hotmail.fr>
2024-10-04 18:22:17 +0200

    add(Ticket): add function buyTicket with timer

--- contracts/contracts/Ticket.sol
+++ contracts/contracts/Ticket.sol
@@ -8,7 +8,10 @@ import "./interfaces/IEncryptedERC20.sol";
 import "./encryptedErc20/EncryptedERC20.sol";
 
 contract Ticket is GatewayCaller, EncryptedERC20 {
+    
     IEncryptedERC20 public token;
+
+    uint256 public endTime = 0;
 
     constructor(
         address _owner,
@@ -18,5 +21,19 @@ contract Ticket is GatewayCaller, EncryptedERC20 {
         address _tokenAddress
     ) EncryptedERC20(_name, _symbol) {
         token = IEncryptedERC20(_tokenAddress);
+    }
+
+    function buyTicket(einput _eUser, einput _eAmount, bytes calldata inputProof) external {
+        require(endTime > 0, "the tombola is not starting ");
+
+        require(block.timestamp <= endTime, "Tombola has ended");
+
+        euint64 eAmount = TFHE.asEuint64(_eAmount, inputProof);
+        TFHE.allow(eAmount, address(this));
+
+        eaddress eUser = TFHE.asEaddress(_eUser, inputProof);
+        TFHE.allow(eUser, address(this));
+
+        euint64 randomNumber = TFHE.randEuint64();
     }
 }
