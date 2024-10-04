93d2e60ce3d9db317bc6e40c5ad6046b18c037bb
jecombe <jeremcombe@hotmail.fr>
2024-10-04 16:51:58 +0200

    add: file Ticket.sol

---
+++ contracts/contracts/Ticket.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "fhevm/lib/TFHE.sol";
+import "fhevm/gateway/GatewayCaller.sol";
+
+contract Ticket is GatewayCaller {
+
+    constructor() {
+    }
+}
