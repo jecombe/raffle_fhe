72c1ed85696d834fc1d3b7e2e1cfa071ae93f618
jecombe <jeremcombe@hotmail.fr>
2024-10-05 01:16:15 +0200

    feat(Ticket): refact require message

--- contracts/contracts/Ticket.sol
+++ contracts/contracts/Ticket.sol
@@ -149,9 +149,9 @@ contract Ticket is GatewayCaller, EncryptedERC20 {
     }
 
     function buyTicket(einput _eUser, einput _eAmount, bytes calldata inputProof) external {
-        require(endTime > 0, "the tombola is not starting ");
+        require(endTime > 0, "the raffle is not starting");
 
-        require(block.timestamp <= endTime, "Tombola has ended");
+        require(block.timestamp <= endTime, "Raffle has ended");
 
         euint64 eAmount = TFHE.asEuint64(_eAmount, inputProof);
         TFHE.allow(eAmount, address(this));
