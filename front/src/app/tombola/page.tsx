cdcbc6903021ff97ee95429c3e376e9e5f4454e5
jecombe <jeremcombe@hotmail.fr>
2024-10-05 21:30:19 +0200

    fix(front): remove dotenv and clear code, remove listener contract

--- front/src/app/tombola/page.tsx
+++ front/src/app/tombola/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
-import { BrowserProvider, ethers, parseEther } from "ethers";
+import { useState } from "react";
 import {
   Container,
   Grid,
@@ -19,7 +18,7 @@ import styles from "../../styles/Tombola.module.css";
 import Link from "next/link";
 import CenterForm from "../components/CenterForm";
 import TicketForm from "../components/TicketsForm";
-import abiFactory from "../../../abi/TicketFactory.json";
+
 
 const Tombola: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -31,52 +30,11 @@ const Tombola: React.FC = () => {
   });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const [entries, setEntries] = useState<{ address: string; reward: number }[]>([]);
+  const [entries, setEntries] = useState<{ address: string; reward: number }[]>(
+    []
+  );
   const [tombolaStarted, setTombolaStarted] = useState(false);
   const [ticketCreated, setTicketCreated] = useState(false);
-  const contractAddress = "0x5a68b96ACC46C627460EaB267e28Ac7c477FdFFE";
-
-
-  const listenForTicketCreated = async () => {
-    if (!window.ethereum) return;
-
-    const provider = new BrowserProvider(window.ethereum);
-    const signer = await provider.getSigner();
-    const contract = new ethers.Contract(contractAddress, abiFactory, signer);
-
-    const ticketCreatedListener = (ticketAddress: string, owner: string) => {
-      console.log("New ticket deployed at:", ticketAddress);
-      console.log("Ticket owner:", owner);
-
-      setTicketCreated(true);
-      const newEntry = {
-        address: ticketAddress,
-        reward: Number(formData.amount) * Number(formData.price),
-      };
-      setEntries((prevEntries) => [...prevEntries, newEntry]);
-
-      setFormData({ ...formData, address: "", amount: "", price: "" });
-    };
-
-    contract.on("TicketCreated", ticketCreatedListener);
-
-    return () => {
-      contract.off("TicketCreated", ticketCreatedListener);
-    };
-  };
-
-  useEffect(() => {
-    listenForTicketCreated();
-
-    return () => {
-   
-    };
-  }, []);
-
-  const handleStartTombola = () => {
-    setTombolaStarted(true);
-    console.log("Tombola started!");
-  };
 
   return (
     <Container maxWidth="md" className={styles.container}>
@@ -95,17 +53,13 @@ const Tombola: React.FC = () => {
 
       <Grid container spacing={3} style={{ marginTop: "20px" }}>
         <Grid item xs={6}>
-          {!ticketCreated ? (
-            <TicketForm
-              onTicketCreated={(address, reward) => {
-                const newEntry = { address, reward };
-                setEntries([...entries, newEntry]);
-                setTicketCreated(true);
-              }}
-            />
-          ) : (
-            <Typography variant="h6">Ticket Created!</Typography>
-          )}
+          <TicketForm
+            onTicketCreated={(address, reward) => {
+              const newEntry = { address, reward };
+              setEntries([...entries, newEntry]);
+              setTicketCreated(true);
+            }}
+          />
         </Grid>
 
         <Grid item xs={6}>
@@ -118,7 +72,6 @@ const Tombola: React.FC = () => {
                 <Button
                   variant="contained"
                   color="secondary"
-                  onClick={handleStartTombola}
                 >
                   Start Tombola
                 </Button>
